9579f520876fbc6e7ffa757a6ff217e3595a8b9c
SHIGERU KAMADO <olomolapraiseoluwadamilare@gmail.com>
2026-04-09 05:36:25 +0100

    fix chrome cache path to project folder

--- .render-build.sh
+++ .render-build.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 set -e
 pnpm install
-PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer node node_modules/puppeteer/install.mjs
-echo "=== Finding Chrome ==="
-find /opt/render/.cache/puppeteer -type f -name "chrome"
+PUPPETEER_CACHE_DIR=/opt/render/project/src/.cache/puppeteer node node_modules/puppeteer/install.mjs
