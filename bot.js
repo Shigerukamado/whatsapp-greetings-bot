1683ab7877fe4bdffafed9519d25bbeff5970d1f
SHIGERU KAMADO <olomolapraiseoluwadamilare@gmail.com>
2026-04-09 04:37:15 +0100

    set chrome executable path for render

--- bot.js
+++ bot.js
@@ -12,6 +12,8 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || 
+            '/opt/render/.cache/puppeteer/chrome/linux-146.0.7680.153/chrome-linux64/chrome',
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     }
 });
