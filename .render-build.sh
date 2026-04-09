21746a104e3a713d8ca973838aa75e806a8766ca
SHIGERU KAMADO <olomolapraiseoluwadamilare@gmail.com>
2026-04-09 05:02:58 +0100

    debug chrome path

--- .render-build.sh
+++ .render-build.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+set -e
 pnpm install
 PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer node node_modules/puppeteer/install.mjs
+echo "=== Finding Chrome ==="
+find /opt/render/.cache/puppeteer -type f -name "chrome"
